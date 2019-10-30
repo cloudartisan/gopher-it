6b92bffac01ce32506fa2d69c6455bc12f315880
David Taylor <>
2019-10-30 20:13:27 +1100

    Adds the first task -- ASCII gopher

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  fmt.Println(time.Now())
+  fmt.Println("      `.-::::::-.`")
+  fmt.Println("  .:-::::::::::::::-:.")
+  fmt.Println("  `_:::    ::    :::_`")
+  fmt.Println("   .:( ^   :: ^   ):.")
+  fmt.Println("   `:::   (..)   :::.")
+  fmt.Println("   `:::::::UU:::::::`")
+  fmt.Println("   .::::::::::::::::.")
+  fmt.Println("   O::::::::::::::::O")
+  fmt.Println("   -::::::::::::::::-")
+  fmt.Println("   `::::::::::::::::`")
+  fmt.Println("    .::::::::::::::.")
+  fmt.Println("      oO:::::::Oo")
+}
